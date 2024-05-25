190883520b8b164fd7e56ae98a1d284136a713aa
Manuel Maisano <manumaisano@gmail.com>
2024-05-25 15:05:48 +0200

    background and dc logo footer

---
+++ resources/views/partials/footermid.blade.php
@@ -0,0 +1,9 @@
+<footer>
+<div id="middlefooter">
+        <div class="container">
+            <div>
+                <img src="{{ Vite::asset('resources/img/dc-logo-bg.png')}}" alt="">
+            </div>
+        </div>
+    </div>
+</footer>
--- resources/views/partials/footertop.blade.php
+++ resources/views/partials/footertop.blade.php
@@ -1,1 +1,18 @@
 
+<footer>
+    <div id="topfooter">
+        <div class="container">
+            <div class="d-flex align-items-center">
+            <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="Logo">
+            <h5>digital comics</h5>
+            <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="Logo">
+            <h5>dc merchandise</h5>
+            <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="Logo">
+            <h5>comic shop locator</h5>
+            <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="Logo">
+            <h5>subscription</h5>
+            </div>
+
+        </div>
+    </div>
+</footer>
